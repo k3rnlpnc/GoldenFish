76b38bcb4103e42fb7c655804af33806d97f2d12
linaShurygina <rocknrolllb20@gmail.com>
2020-05-04 22:22:44 +0300

    Оформление класса-сущности

--- GoldenFish/models/User.py
+++ GoldenFish/models/User.py
@@ -1,17 +1,31 @@
-class User:
-    def __init__(self, _email, _password, _username, _name, _surname, _dob):
+from sqlalchemy import Column, Integer, String, Date
+from config import Base
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(40))
+    password = Column(String(20))
+    username = Column(String(30))
+    name = Column(String(20))
+    surname = Column(String(20))
+    birthday = Column(Date, nullable=True)
+
+    def __init__(self, _email, _password, _username, _name, _surname, _dob=None):
         self.email = _email
         self.password = _password
         self.username = _username
         self.name = _name
         self.surname = _surname
-        self.date_of_birth = _dob
+        self.birthday = _dob
 
-    def get_id(self):  # id <- autoincrement
-        pass
+    def get_id(self) -> int:
+        return self.id
 
-    def get_email(self):
-        pass
+    def get_email(self) -> str:
+        return self.email
 
-    def set_email(self, email):
-        pass
+    def set_email(self, _email):
+        self.email = _email
