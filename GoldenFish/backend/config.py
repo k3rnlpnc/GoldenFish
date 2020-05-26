0c4968a9654cd5361c44870c680c5c38428e7883
linaShurygina <rocknrolllb20@gmail.com>
2020-05-26 18:36:48 +0300

    Добавила токен авторизации

--- GoldenFish/backend/config.py
+++ GoldenFish/backend/config.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('postgresql://postgres:fyrfyr@localhost:5432/goldenfish')
-Session = sessionmaker(bind=engine)
 
-Base = declarative_base()
+
+class SecurityConfig:
+    SECRET_KEY = 'salt4567$%&'
