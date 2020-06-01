440b379366f61f89de0f7f40815d99adceefb9cc
linaShurygina <rocknrolllb20@gmail.com>
2020-06-01 19:34:41 +0300

    Тесты для модулей регистрации/авторизации

--- GoldenFish/backend/test.py
+++ GoldenFish/backend/test.py
@@ -1,17 +1,44 @@
 import pytest
+from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
 from backend.app import client
 from backend.models.Dream import Dream
 
 
-def test_get_dreams():
-    res = client.get('/mywishes/1')
+def test_register():
+    data = {
+        'name': 'Name',
+        'surname': 'Surname',
+        'email': 'someemail@mail.com',
+        'username': 'Username',
+        'password': 'qwerty'
+    }
+    res = client.post('/registration', json=data)
 
     assert res.status_code == 200
-    assert len(res.get_json()) == len(Dream.query.filter_by(owner_id=1).all())
-    assert res.get_json()[0]['id'] == 1
 
 
+def test_authenticate():
+    data = {
+        'email': 'someemail@mail.com',
+        'password': 'qwerty'
+    }
+    res = client.post('/authentication', json=data)
+    assert res.status_code == 200
+
+
+def test_get_dreams():
+    headers = {
+        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1OTEwMTUyNDgsIm5iZiI6MTU5MTAxNTI0OCwianRpIjoiYjQ1N2VlYjktODkyNS00MmQ1LWIxODAtNDI0NDBmYWIxMmRkIiwiZXhwIjoxNTkzMDg4ODQ4LCJpZGVudGl0eSI6NywiZnJlc2giOmZhbHNlLCJ0eXBlIjoiYWNjZXNzIn0.EXKctq1DY2kqrXwTlBsUAuV775JV9JHu7TVVJMDFEh0'
+    }
+    res = client.get('/mywishes', headers=headers)
+    user_id = get_jwt_identity()
+
+    assert res.status_code == 200
+    assert len(res.get_json()) == len(Dream.query.filter_by(owner_id=user_id).all())
+
+'''
 def test_put_dream():
+    
     dream = {
         'name': 'Путешествие на Марс',
         'owner_id': 1
@@ -34,3 +61,4 @@ def test_delete_dream():
 
     assert res.status_code == 204
     assert Dream.query.get(3) is None
+'''
