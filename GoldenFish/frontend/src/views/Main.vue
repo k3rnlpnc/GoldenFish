21e7590113908887f2f0881bed81cdd577f1ca0c
LeprechaunSN <sveta.nikonova.2014@mail.ru>
2020-12-14 13:36:25 +0300

    Добавлена компонента Main

---
+++ GoldenFish/frontend/src/views/Main.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Main',
+    mounted() {
+        document.title = "Вход";
+        
+        if (this.$store.state.auth.status.loggedIn) {
+            this.$router.push('/mywishes');
+        }
+        else {
+            this.$router.push('/home');
+        }
+    }
+}
+</script>
