cfe8039bf9228bc9cd1c16c82b1b6c03a0c6568a
LeprechaunSN <sveta.nikonova.2014@mail.ru>
2020-12-14 13:50:55 +0300

    Добавлена ссылка на развернутый backend

--- GoldenFish/frontend/src/services/auth.service.js
+++ GoldenFish/frontend/src/services/auth.service.js
@@ -1,41 +1,50 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:5000/';
+const API_URL = 'https://golden-fish-api.herokuapp.com/';
 
 class AuthService {
-  login(user) {
-    return axios
-      .post(API_URL + 'authentication', {
+    login(user) {
+        return axios.post(API_URL + 'authentication', {
+            email: user.email,
+            password: user.password
+        })
+        .then(
+            response => {
+                if (response.data.access_token) {
+                    localStorage.setItem('user', JSON.stringify(response.data.access_token));
+                }
+                return Promise.resolve(response.data);
+            }, 
+            error => {
+                return Promise.reject(error);
+            }
+        );
+    }
+
+    logout() {
+        localStorage.removeItem('user');
+    }
+
+    register(user) {
+        return axios.post(API_URL + 'registration', {
         email: user.email,
-        password: user.password
-      })
-      .then(
-        response => {
-          if (response.data.access_token) {
-            localStorage.setItem('user', JSON.stringify(response.data.access_token));
-          }
-          // TODO: should it be: return response.data.access_token ?
-          return Promise.resolve(response.data);
-        }, 
-        error => {
-          return Promise.reject(error);
-      });
-  }
-
-  logout() {
-    localStorage.removeItem('user');
-  }
-
-  register(user) {
-    return axios.post(API_URL + 'registration', {
-      email: user.email,
-      password: user.password,
-      username: user.username,
-      name: user.name,
-      surname: user.surname,
-      birthday: user.birthday
-    });
-  }
+        password: user.password,
+        username: user.username,
+        name: user.name,
+        surname: user.surname,
+        birthday: user.birthday
+        }).then(
+            response => {
+                if (response.data.access_token) {
+                    localStorage.setItem('user', JSON.stringify(response.data.access_token));
+                }
+                return Promise.resolve(response.data);
+            }, 
+            error => {
+                return Promise.reject(error);
+            }
+        );
+    }
 }
 
 export default new AuthService();
--- GoldenFish/frontend/src/services/dream.service.js
+++ GoldenFish/frontend/src/services/dream.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'http://127.0.0.1:5000/';
+const API_URL = 'https://golden-fish-api.herokuapp.com/';
 
 class DreamService {
     getMyWishes() {
